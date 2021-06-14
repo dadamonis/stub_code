176715d88fa6a4c20430941e4a8898373d244cb5
dadamonis <dadamonis@users.noreply.github.com>
2021-06-14 11:34:12 -0400

    Create ms_flow_email_automation.py

---
+++ ms_flow_email_automation.py
@@ -0,0 +1,31 @@
+import requests
+
+def send_email_notification( ) -> None:
+    """
+    This function will send an email via MS Flow.
+      url - Provided by MSFlow once the flow has been created
+      emailaddress - the email account that is SENDING the message
+      emaiSubject - the subject for the email that is being sent by the flow
+      emailBody - the message that the email will contain
+      errors - additional messages that will be included in the email
+    
+    Parameters
+    ==========
+    None
+        
+    Results
+    None
+    ==========
+    """
+    url = '<your_msflow_url>'
+
+    headers = {'Content-type': 'application/json'}
+    body ={'emailaddress': '',
+           'emailSubject':'',
+           'emailBody':f"""<your_message>: """,
+           'errors':f"""<your_error_messages>"""
+          }
+
+    r = requests.post(url, 
+                      headers=headers,
+                      json = body)
